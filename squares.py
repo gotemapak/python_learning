e9c57caabaa4afb92eca98c1bdd9dfab60a8a384
Артем Пак <artempak_macbook_air@Artyoms-MacBook-Air.local>
2025-01-11 19:29:19 +0300

    add files after modules and pocket lesson

---
+++ squares.py
@@ -0,0 +1,15 @@
+import functions_refactored
+
+x = [1, 5, 65, 1, 6]
+
+def square(x):
+    return x**2
+
+xx = list(map(lambda x: x**2, x)) #lambda делает тоже самое, 
+#                                  что делает def
+
+xx = list(map(square, x)) 
+
+print(xx)
+
+# 1, 25 4225
